ada90b4bffa9acf428f21ece6467d9ddb6f26b3b
Tomokazu Kiyohara <tomokazu.kiyohara@gmail.com>
2013-05-23 17:36:22 +0900

    formalize app.json w/ jshint

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+'use strict';
 
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path');
 
 var app = express();
 
@@ -31,5 +32,5 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
